fdb1e5d9da87ed6889aaf83a0552815d48dd1418
WuHan <2749514521@qq.com>
2025-11-19 11:50:54 +0800

    创建工程

---
+++ components/test01/test01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "test01.h"
+
+void func(void)
+{
+    printf("hello world\n");
+}
---
+++ main/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test01.h"
+void app_main(void)
+{
+    func();
+}
